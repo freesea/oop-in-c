c22c4d3853ad4365b867f7af2edc6cb139574ae5
502644154@qq.com <password890>
2018-04-01 15:16:37 +0800

    1 Encapsulation 封装

---
+++ shape.c
@@ -0,0 +1,16 @@
+//Listing 2 Definition of the Shape “class” in C (file shape.c)
+
+#include "shape.h"/* Shape class interface */
+
+/* constructor implementation */
+void Shape_ctor(Shape * const me,int16_t x,int16_t y)
+{
+	me->x=x;
+	me->y=y;
+}
+/* move-by operation implementation */
+void Shape_moveBy(Shape * const me,int16_t dx,int16_t dy)
+{
+	me->+=dx;
+	me->+=dy;
+}
